613fcb7eed17b071b9aa0d3eb485b44d8e448148
AndreasBoegh <32385164+AndreasBoegh@users.noreply.github.com>
2018-04-26 13:21:45 +0200

    CaseWorker

--- SensumUdred_Business/src/business/logic/Case.java
+++ SensumUdred_Business/src/business/logic/Case.java
@@ -9,7 +9,7 @@ import java.util.Date;
  * @author andreasmolgaard-andersen
  */
 public class Case implements ICase {
-    
+
     private boolean informed;
     private String state;
     private Date openingDate;
@@ -19,29 +19,28 @@ public class Case implements ICase {
     private String reason;
     private String availableOffers;
     private String sourceOfReqeust;
-    
     private Citizen citizen;
 
     public Case() {
-        
+
     }
 
     public Case(ICase c) {
-        
+
     }
-    
+
     public int getCitizenCPR() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     public String getCitizenName() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     public boolean closeCase() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public ICitizen getCitizen() {
         return citizen;
@@ -76,7 +75,5 @@ public class Case implements ICase {
     public String getSourceOfRequest() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 
-    
 }
--- SensumUdred_Business/src/business/logic/CaseWorker.java
+++ SensumUdred_Business/src/business/logic/CaseWorker.java
@@ -1,9 +1,116 @@
 package business.logic;
 
+import common.ICase;
+import common.ICaseWorker;
+import common.ICitizenData;
+
 /**
  *
  * @author andreasmolgaard-andersen
  */
-public class CaseWorker {
+public class CaseWorker extends Person implements ICaseWorker{
+
+    /**
+     * 
+     */
+    private int employeeId;
+    
+    /**
+     * 
+     */
+    private String userId;
+    
+    /**
+     * 
+     */
+    private Case[] cases;
+    
+    /**
+     * 
+     * @param name
+     * @param mobilNumber
+     * @param email 
+     */
+    public CaseWorker(String name, int mobilNumber, String email) {
+        super(name, mobilNumber, email);
+    }
+    
+    /**
+     *
+     * @param caseWorker
+     */
+    public CaseWorker(ICaseWorker caseWorker) {
+        super(caseWorker.getName(), caseWorker.getMobileNumber(), caseWorker.getEmail());
+        
+    }
+
+    /**
+     * 
+     * @param data
+     * @return 
+     */
+    public Case openCase(ICitizenData data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * 
+     * @param caseId
+     * @return 
+     */
+    public boolean closeCase(int caseId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean editCase(ICitizenData data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public boolean saveCase() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * 
+     * @param c
+     * @return 
+     */
+    public boolean editCase(ICase c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    } 
+    
+    /**
+     * 
+     * @return 
+     */
+    public ICase[] getActiveCases() {
+        return cases;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public int getEmployeeId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public String getUserId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
