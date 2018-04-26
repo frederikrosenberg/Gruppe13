af0943d339b49cd7be504ff18c24a0b8c7006812
Krongrah <Krongrah@gmail.com>
2018-04-26 13:18:42 +0200

    User Class
    The user class.

--- SensumUdred_Business/src/business/security/User.java
+++ SensumUdred_Business/src/business/security/User.java
@@ -5,10 +5,113 @@
  */
 package business.security;
 
+import common.IUser;
+import common.Role;
+
 /**
+ * A registered user of the system
  *
  * @author Krongrah
  */
-public class User {
-    
+public class User implements IUser {
+
+    /**
+     * The name of the user
+     */
+    private String name;
+    /**
+     * The id of the user
+     */
+    private String userId;
+    /**
+     * The username of the user
+     */
+    private String username;
+    /**
+     * The hashed password of the user
+     */
+    private String password;
+    /**
+     * The role of the user; either case worker or citizen
+     */
+    private Role role;
+
+    /**
+     * True if the the user has access to the system. False of the user has lost
+     * permission to the system.
+     */
+    private boolean active = true;
+
+    /**
+     * The constructor of the user class
+     *
+     * @param name the name of the user
+     * @param username the username of the user
+     * @param password the hashed password of the user
+     * @param role the role of the user; either case worker or citizen
+     * @param userId The id of the user
+     */
+    public User(String name, String username, String password, Role role, String userId) {
+        this.name = name;
+        this.username = username;
+        this.role = role;
+        this.password = password;
+        this.userId = userId;
+    }
+
+    /**
+     * Returns the role of the user.
+     * @return the role of the user
+     */
+    @Override
+    public Role getRole() {
+        return role;
+    }
+/**
+ * sets the user to deactivated, removing their access to the system
+ */
+    public void deactivateUser() {
+        active = false;
+    }
+/**
+ * Returns the username of the user.
+ * @return the username of the user
+ */
+    @Override
+    public String getUsername() {
+        return username;
+    }
+/**
+ *  Returns the name of the user.
+ * @return the name of the user
+ */
+    @Override
+    public String getName() {
+        return name;
+    }
+/**
+ * Returns the id of the user,
+ * @return the id of the user
+ */
+    @Override
+    public String getUserId() {
+        return userId;
+    }
+/**
+ * Returns the hashed password of the user.
+ * @return the hashed password of the user
+ */
+    @Override
+    public String getPassword() {
+        return password;
+    }
+/**
+ * Returns true of the user is active, and thus has access to the system, else returns false.
+ * @return true of the user is active, and thus has access to the system, else returns false
+ */
+    @Override
+    public boolean getActive() {
+        return active;
+    }
+
 }
