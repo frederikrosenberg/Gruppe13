33041ac8c7331f66dfb72140c2d0471b0b5c1d6c
Krongrah <Krongrah@gmail.com>
2018-04-26 14:01:43 +0200

    Filled user manager
    filled user manager. untested-

--- SensumUdred_Business/src/business/security/UserManager.java
+++ SensumUdred_Business/src/business/security/UserManager.java
@@ -1,9 +1,11 @@
-
 package business.security;
 
 import common.IUser;
 import common.IUserManager;
+import common.Role;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  *
@@ -11,9 +13,31 @@ import java.util.List;
  */
 public class UserManager implements IUserManager {
 
+    private List<User> users = new ArrayList();
+
     @Override
-    public List<IUser> getUsers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<? extends IUser> getUsers() {
+        return users;
     }
-    
+
+    public void addUser(String name, String username, String password, Role role) {
+        users.add(new User(name, username, password, role, generateId()));
+
+    }
+
+    private String generateId() {
+        String userId;
+        loop:
+        while (true) {
+            userId = UUID.randomUUID().toString();
+            for (User user : users) {
+                if (user.getUserId().equals(userId)) {
+                    continue loop;
+                }
+            }
+            break;
+        }
+        return userId;
+    }
+
 }
