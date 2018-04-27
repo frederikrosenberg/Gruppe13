a4bdc636366ff3d21d964031dbfda302607b6e39
Krongrah <Krongrah@gmail.com>
2018-04-27 11:15:17 +0200

    added security facade interface
    added security facade interface

--- SensumUdred_Business/src/business/common/ISecurityFacade.java
+++ SensumUdred_Business/src/business/common/ISecurityFacade.java
@@ -6,9 +6,28 @@
 package business.common;
 
 /**
+ * The interface for the security facade.
  *
  * @author mikkellarsen
  */
 public interface ISecurityFacade {
-    
+
+    /**
+     * logs the user into the system if the username and password entered
+     * matches that of an existing user.
+     *
+     * @param username the username of the user attempting to log in
+     * @param password the password of the user attempting to log in
+     * @return true if the username and password both matches a registered user,
+     * false otherwise
+     */
+    public boolean logIn(String username, String password);
+
+    /**
+     * Logs the user out of the system.
+     *
+     * @return true if the log out was successful, otherwise false.
+     */
+
+    public boolean logOut();
 }
