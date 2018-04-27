5df46999e1f69ef1a6dcb122697d758cabf45e48
Krongrah <Krongrah@gmail.com>
2018-04-27 10:50:28 +0200

    Finished the UserManager
    Finished the user manager.

--- SensumUdred_Business/src/business/security/UserManager.java
+++ SensumUdred_Business/src/business/security/UserManager.java
@@ -8,23 +8,41 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- *
+ * The class that creates and stores all the usrs of the program
  * @author Krongrah
  */
 public class UserManager implements IUserManager {
 
+    /**
+     * The a list containing all the users of the program.
+     */
     private List<User> users = new ArrayList();
 
+    /**
+     * Returns the list of users of the program.
+     * @return the list of users of the program
+     */
     @Override
     public List<? extends IUser> getUsers() {
         return users;
     }
 
+    /**
+     * Adds a new user to the program.
+     * @param name the name of the user
+     * @param username the username of the user
+     * @param password the hashed password of the user
+     * @param role the role of the user
+     */
     public void addUser(String name, String username, String password, Role role) {
         users.add(new User(name, username, password, role, generateId()));
 
     }
-
+    
+    /**
+     * Generates a unique 36 digit id.
+     * @return a unique 36 digit id
+     */
     private String generateId() {
         String userId;
         loop:
