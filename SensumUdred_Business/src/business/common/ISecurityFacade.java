6341164d79032187394129a122472ccc1126ae4f
Krongrah <Krongrah@gmail.com>
2018-04-30 14:43:59 +0200

    tiny fixes
    tiny fixes

--- SensumUdred_Business/src/business/common/ISecurityFacade.java
+++ SensumUdred_Business/src/business/common/ISecurityFacade.java
@@ -10,7 +10,12 @@ import common.Role;
 /**
  * The interface for the security facade.
  *
- * @author mikkellarsen
+ * @author Andreas Mølgaard-Andersen
+ * @author Lars Bjerregaard Jørgensen
+ * @author Frederik Rosenberg
+ * @author Mikkel Larsen
+ * @author Sebastian Christensen
+ * @author Kasper Schødts
  */
 public interface ISecurityFacade {
 
