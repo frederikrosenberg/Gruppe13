0e3bb150d994c6c97fe775edb3d674dbadec942e
Lars Jørgensen <lars.b.jn@gmail.com>
2018-04-30 15:16:41 +0200

    Fixed IPerson
    * Fixed IPerson

--- SensumUdred_Common/src/common/IPerson.java
+++ SensumUdred_Common/src/common/IPerson.java
@@ -6,11 +6,32 @@
 package common;
 
 /**
- *
- * @author fsr19
+ * @author Andreas Mølgaard-Andersen
+ * @author Lars Bjerregaard Jørgensen
+ * @author Frederik Rosenberg
+ * @author Mikkel Larsen
+ * @author Sebastian Christensen
+ * @author Kasper Schødts
  */
 public interface IPerson {
+    /**
+     * Get the name of the person
+     *
+     * @return name
+     */
     String getName();
-    String  getPhoneNumber();
+ 
+    /**
+     * Get the mobile number of the person
+     *
+     * @return phone number
+     */
+    String getPhoneNumber();
+ 
+    /**
+     * Get the email of the person;
+     *
+     * @return email
+     */
     String getEmail();
 }
