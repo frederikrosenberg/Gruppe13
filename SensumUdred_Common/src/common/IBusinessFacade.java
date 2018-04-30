c710ca506241188ff0afeb134f81c67028c3e782
Lars Jørgensen <lars.b.jn@gmail.com>
2018-04-30 12:42:14 +0200

    Removed "-" from javadoc
    * Removed "-" from javadoc

--- SensumUdred_Common/src/common/IBusinessFacade.java
+++ SensumUdred_Common/src/common/IBusinessFacade.java
@@ -16,11 +16,11 @@ public interface IBusinessFacade {
     boolean closeCase();
     void closing();
      /**
- -     * Find active cases from either cpr or case id 
- -     * @param value the search parameter
- -     * @param isCpr if true the value is cpr otherwise it is case id 
- -     * @return the given case that is found, if no case found null is returned
- -     */
+      * Find active cases from either cpr or case id 
+      * @param value the search parameter
+      * @param isCpr if true the value is cpr otherwise it is case id 
+      * @return the given case that is found, if no case found null is returned
+      */
     ICase findActiveCase(int value, boolean isCpr);
     ICase findActiveCase(String name);
     ICase[] getAllActiveCases();
