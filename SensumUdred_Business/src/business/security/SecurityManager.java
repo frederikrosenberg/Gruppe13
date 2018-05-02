54db29f408beaac66572deba8628a797b69f4434
Frederik Rosenberg <fsr1998@hotmail.com>
2018-05-03 01:20:38 +0200

    Updated hasher to use a better algorithm

--- SensumUdred_Business/src/business/security/SecurityManager.java
+++ SensumUdred_Business/src/business/security/SecurityManager.java
@@ -54,14 +54,9 @@ public class SecurityManager {
      * @return user id if the login was successful, otherwise null
      */
     public String logIn(String username, String password) {
-        if (!hasher.isReady()) {
-            System.err.println("Hasher not ready");
-            return null;
-        }
-        String hashedPassword = hashPassword(password);
         for (IUser user : userManager.getUsers()) {
             if (user.getActive() && user.getUsername().equals(username)
-                    && user.getPassword().equals(hashedPassword)) {
+                    && hasher.compare(password, user.getPassword())) {
                 currentUser = user;
                 return user.getUserId();
             }
