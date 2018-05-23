63b0170205eab40fae4fe3bfa534df103e00fb4c
Lars Jørgensen <lars.b.jn@gmail.com>
2018-05-23 14:08:39 +0200

    Added CaseState enum
    * Added CaseState enum

---
+++ SensumUdred_Common/src/common/CaseState.java
@@ -0,0 +1,16 @@
+package common;
+
+/**
+ * Contains case states
+ *
+ * @author Andreas Mølgaard-Andersen
+ * @author Lars Bjerregaard Jørgensen
+ * @author Frederik Rosenberg
+ * @author Mikkel Larsen
+ * @author Sebastian Christensen
+ * @author Kasper Schødts
+ */
+public enum CaseState {    
+    OPEN,
+    CLOSED
+}
